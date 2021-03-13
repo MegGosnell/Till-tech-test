723245dac608a1d1ab53bd26513b025863db39b1
megan gosnell <megan_gosnell@aol.co.uk>
2021-03-13 11:23:33 +0000

    first basic test passing

--- lib/till.rb
+++ lib/till.rb
@@ -1,2 +1,6 @@
 class Till 
+
+    def order
+        "latte"
+    end 
 end 
--- spec/till_spec.rb
+++ spec/till_spec.rb
@@ -1,4 +1,10 @@
 require 'till'
 
 describe Till do 
+subject(:till) {described_class.new}
+    describe "#order" do 
+        it "shows the items that have been ordered" do 
+            expect(subject.order).to eq "latte"
+        end 
+    end
 end 
