a8a2d5b1655ec004a543f5de89f67bb2a60a2d15
megan gosnell <megan_gosnell@aol.co.uk>
2021-03-13 11:30:34 +0000

    initialize method with item, name and quantity as instances created

--- lib/till.rb
+++ lib/till.rb
@@ -1,6 +1,16 @@
 class Till 
-
+attr_reader :quantity, :item, :name
+    def initialize
+        @item = item 
+        @quantity = 0
+        @name = name
+    end 
+    
     def order
         "latte"
     end 
+
+    def receipt 
+        @quantity 
+    end
 end 
--- spec/till_spec.rb
+++ spec/till_spec.rb
@@ -2,6 +2,13 @@ require 'till'
 
 describe Till do 
 subject(:till) {described_class.new}
+
+    describe "#initialize" do 
+        it 'should create an instance' do 
+            expect(subject.receipt).to eq 0
+        end 
+    end 
+
     describe "#order" do 
         it "shows the items that have been ordered" do 
             expect(subject.order).to eq "latte"
